9d0895a77c607aaed0a4a8277bcf6618d689ec4d
Soapi14 <gdsix4ate9@gmail.com>
2026-03-26 19:56:21 -0400

    Implementing Poincare sections

--- classical/computational/question2.py
+++ classical/computational/question2.py
@@ -15,8 +15,9 @@ omega = 1.5
 amp = 0.6
 
 # Define simulation parameters
-t_span = (0, 500)
+t_span = (0, 1000)
 t_points = np.linspace(t_span[0], t_span[1], int(1e4))
+poincare_points = np.arange(t_span[0], t_span[1], 2 * np.pi)
 
 
 def duffing(t, y):
@@ -42,9 +43,28 @@ sol = solve_ivp(duffing, t_span, y0, dense_output=True)
 
 # Use the continuous solution for plotting
 y = sol.sol(t_points)
+poincare_y = sol.sol(poincare_points)
 
-# Phase plot of the results
-plt.plot(y[0], y[1], "-")
+# Poincare Section
+plt.plot(poincare_y[0], poincare_y[1], "o", markersize=1)
 plt.xlabel("Position")
 plt.ylabel("Velocity")
-save_show_plots("phase_plot2")
+save_show_plots(f"poincare_section_{amp}")
+
+# # Time series of position
+# plt.plot(t_points, y[0], "-")
+# plt.xlabel("Time")
+# plt.ylabel("Position")
+# save_show_plots("time_series")
+
+# # Time series of velocity
+# plt.plot(t_points, y[1], "-")
+# plt.xlabel("Time")
+# plt.ylabel("Velocity")
+# save_show_plots("time_series_velocity")
+
+# # Phase plot of the results
+# plt.plot(y[0], y[1], "-")
+# plt.xlabel("Position")
+# plt.ylabel("Velocity")
+# save_show_plots(f"phase_plot_driven_{amp}")
